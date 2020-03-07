ad04ee8878b728b97b0960def6ca511ae7f51443
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-03-07 18:35:19 -0300

    button added

--- src/GUI/interface.py
+++ src/GUI/interface.py
@@ -5,5 +5,11 @@ root = Tk()
 label = Label(root, text="Hello RO!")
 label.pack()
 
+#button
+def loadImage():
+    print("loading image")
+
+addImageButton = Button(root, text="Load Image", fg="blue", command=loadImage).pack();
+
 #main loop
 root.mainloop()
