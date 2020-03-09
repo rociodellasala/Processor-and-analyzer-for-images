e5b05d65f6d8140c52e937d9ec62f003efb3fff9
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-03-09 11:33:09 -0300

    simply image load added

--- src/GUI/interface.py
+++ src/GUI/interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
 # label
@@ -9,7 +10,11 @@ label.pack()
 def loadImage():
     print("loading image")
 
-addImageButton = Button(root, text="Load Image", fg="blue", command=loadImage).pack();
+addImageButton = Button(root, text="Load Image", fg="blue", command=loadImage).pack()
 
+#image
+image = ImageTk.PhotoImage(Image.open('../../images/Lenaclor.ppm'))
+imageLabel = Label(image=image)
+imageLabel.pack();
 #main loop
 root.mainloop()
