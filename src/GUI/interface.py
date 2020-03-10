b30de20088364834314bb9fcc5b5ec843118c677
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-03-10 11:47:22 -0300

    reading image from bytes, but image not showing correctly

--- src/GUI/interface.py
+++ src/GUI/interface.py
@@ -7,7 +7,14 @@ root = Tk()
 label = Label(root, text="Hello RO!")
 label.pack()
 
-def openFileName():
+def read_raw_image(path):
+    with open(path, "rb") as binary_file:
+        # Read the whole file at once
+        raw_image = binary_file.read()
+        return raw_image
+
+
+def open_file_name():
     file_name = filedialog.askopenfilename(title='Choose Image', filetypes=[("ppm", "*.ppm"), ("pgm", "*.pgm"),
                                                                             ("jpg", "*.jpg"), ("png", "*.png"),
                                                                             ("jpeg", "*.jpeg"),("raw", "*.RAW")])
@@ -18,11 +25,16 @@ def openFileName():
 
 
 def load_image():
-    file_name = openFileName()
+    file_name = open_file_name()
     if file_name:
-        # opens the image
-        image = Image.open(file_name)
-
+        if file_name.endswith(".RAW"):
+            raw_image = read_raw_image(file_name)
+            print(raw_image)
+            # TODO if extension is RAW do this
+            image = Image.frombytes('1', (290, 207), raw_image)
+        else:
+            # opens the image
+            image = Image.open(file_name)
         # resize the image and apply a high-quality down sampling filter
         image = image.resize((512, 512), Image.ANTIALIAS)
 
@@ -35,7 +47,6 @@ def load_image():
         # set the image as img
         panel.image = image
         panel.pack()
-
 
 add_image_button = Button(root, text="Load Image", fg="blue", command=load_image).pack()
 
