ee3f0946fc3db4551a4eb36a4527d180c5ff7815
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-03-10 10:05:51 -0300

    open image with button

--- src/GUI/interface.py
+++ src/GUI/interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from PIL import ImageTk,Image
 
 root = Tk()
@@ -6,15 +7,35 @@ root = Tk()
 label = Label(root, text="Hello RO!")
 label.pack()
 
-#button
+def openFileName():
+    filename = filedialog.askopenfilename(title='"pen')
+    return filename
+
 def loadImage():
     print("loading image")
+    fileName = openFileName()
+    print(fileName)
+    # opens the image
+    image = Image.open(fileName)
+    # # resize the image and apply a high-quality down sampling filter
+    # img = img.resize((250, 250), Image.ANTIALIAS)
+
+    # PhotoImage class is used to add image to widgets, icons etc
+    image = ImageTk.PhotoImage(image)
+
+    # create a label
+    panel = Label(root, image=image)
+
+    # set the image as img
+    panel.image = image
+    panel.pack()
 
 addImageButton = Button(root, text="Load Image", fg="blue", command=loadImage).pack()
 
-#image
-image = ImageTk.PhotoImage(Image.open('../../images/Lenaclor.ppm'))
-imageLabel = Label(image=image)
-imageLabel.pack();
+# #image
+# image = ImageTk.PhotoImage(Image.open('../../images/Lenaclor.ppm'))
+# imageLabel = Label(image=image)
+# imageLabel.pack()
+
 #main loop
 root.mainloop()
