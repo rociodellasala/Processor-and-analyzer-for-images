7867bf2ab2ae6871725ec0bcdae9e744c1b2483b
Agustin Izaguirre <Agustinizag1@gmail.com>
2020-04-23 23:43:41 -0300

    four direction detection input added

--- src/GUI/border_detection_menu.py
+++ src/GUI/border_detection_menu.py
@@ -1,10 +1,11 @@
-from tkinter import Menu, Entry, messagebox, ttk, Radiobutton, StringVar
+from tkinter import Menu, Entry, messagebox, ttk, Radiobutton, StringVar, IntVar
 from src.GUI import gui_constants as constants
 from src.GUI.interface_info import InterfaceInfo
 from border_detectors import prewit_detection, sobel_detection, \
     prewit_color_detection, sobel_color_detection
 from filters import border_enhancement_filter
-from border_detectors import laplacian_method, laplacian_method_with_slope_evaluation, laplacian_gaussian_method
+from border_detectors import laplacian_method, laplacian_method_with_slope_evaluation,\
+    laplacian_gaussian_method, four_direction_border_detection
 
 
 def prewit_detection_wrapper():
@@ -37,6 +38,31 @@ def sobel_color_detection_wrapper():
         messagebox.showerror(title="Error", message="You must upload an image to detect borders")
     else:
         sobel_color_detection(interface.current_image, constants.HEIGHT, constants.WIDTH)
+
+
+def four_direction_input():
+    interface = InterfaceInfo.get_instance()
+    if interface.current_image is None:
+        messagebox.showerror(title="Error", message="You must upload an image to detect borders")
+    else:
+        # ttk.Label(interface.buttons_frame, text="Prewitt", background=constants.TOP_COLOR).grid(row=0, column=0)
+        # ttk.Label(interface.buttons_frame, text="Sobel", background=constants.TOP_COLOR).grid(row=1, column=0)
+        # ttk.Label(interface.buttons_frame, text="Kirsh", background=constants.TOP_COLOR).grid(row=0, column=2)
+        radio_var = IntVar()
+        radio_var.set(1)
+        Radiobutton(interface.buttons_frame, text="Prewitt", value=2, variable=radio_var,
+                    background=constants.TOP_COLOR).grid(row=1, column=0)
+        Radiobutton(interface.buttons_frame, text="Sobel", value=1, variable=radio_var,
+                    background=constants.TOP_COLOR).grid(row=1, column=1)
+        Radiobutton(interface.buttons_frame, text="Kirsh", value=3, variable=radio_var,
+                    background=constants.TOP_COLOR).grid(row=1, column=2)
+
+        detect_borders_button = ttk.Button(interface.buttons_frame, text="Detect Borders",
+                                           command=lambda:four_direction_border_detection(interface.current_image,
+                                                                                          constants.HEIGHT,
+                                                                                          constants.WIDTH,
+                                                                                          radio_var.get()))
+        detect_borders_button.grid(row=2, column=0)
 
 
 def border_enhancement_filter_wrapper(image, width, height):
@@ -128,6 +154,10 @@ class BorderDetectionMenu:
         interface = InterfaceInfo.get_instance()
         border_detection_menu = Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Border detection", menu=border_detection_menu)
+        border_detection_menu.add_command(label="Border enhancement",
+                                          command=lambda: border_enhancement_filter_wrapper(interface.current_image,
+                                                                                            constants.WIDTH,
+                                                                                            constants.HEIGHT))
         prewit_menu = Menu(border_detection_menu, tearoff=0)
         border_detection_menu.add_cascade(label="Prewitt", menu=prewit_menu)
         prewit_menu.add_command(label="Color", command=prewit_color_detection_wrapper)
@@ -136,9 +166,7 @@ class BorderDetectionMenu:
         border_detection_menu.add_cascade(label="Sobel", menu=sobel_menu)
         sobel_menu.add_command(label="Color", command=sobel_color_detection_wrapper)
         sobel_menu.add_command(label="B&W", command=sobel_detection_wrapper)
-        border_detection_menu.add_command(label="Border enhancement",
-                                 command=lambda: border_enhancement_filter_wrapper(interface.current_image,
-                                                                                   constants.WIDTH, constants.HEIGHT))
+        border_detection_menu.add_command(label="Four directions", command=four_direction_input)
         border_detection_menu.add_command(label="Laplacian", command=generate_laplacian_input)
         border_detection_menu.add_command(label="Laplacian Gaussian", command=generate_laplacian_gaussian_input)
         border_detection_menu.add_command(label="Laplacian with slope", command=generate_laplacian_with_slope_input)
